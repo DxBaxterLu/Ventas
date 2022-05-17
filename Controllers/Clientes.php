1503a53b7c55a5734d06970b9bf8bf1640f1863f
Baxter <dave.f7.mart@gmail.com>
2022-05-16 23:48:51 -0500

    modificaciones

--- Controllers/Clientes.php
+++ Controllers/Clientes.php
@@ -8,24 +8,71 @@ class Clientes extends Controller
 
     public function index()
     {
+        $i = 0;
+        $data = "";
+
+        
         $this->views->getView($this, "index");
+        $data[$i]['acciones'] = '<div>
+                <button class="btn btn-primary" type="button" onclick="btnEditarUser(' . $data[$i]['id'] . ');"><i class="fas fa-edit"></i></button>
+                <button class="btn btn-danger" type="button" onclick="btnEliminarUser(' . $data[$i]['id'] . ');"><i class="fas fa-trash-alt"></i></button>
+                <div/>';
     }
 
-    /*public function validar()
+    public function registrar()
     {
-        if (empty($_POST['nombre'] || $_POST['apellido'] || $_POST['cedula'] || $_POST['telefono'] || $_POST['correo'] || $_POST['direccion'])) {
-            $msg = "Los campos estan vacios";
-        } else {
-            $nombre = $_POST['nombre'];
-            $apellido = $_POST['apellido'];
-            $cedula = $_POST['cedula'];
-            $telefono = $_POST['telefono'];
-            $correo = $_POST['correo'];
-            $direccion = $_POST['direccion'];
-            $data = $this->model->getClientes($nombre, $apellido, $cedula, $telefono, $correo, $direccion);
+        $usuario = $_POST['usuario'];
+        $nombre = $_POST['nombre'];
+        $clave = $_POST['clave'];
+        $confirmar = $_POST['confirmar'];
+        $caja = $_POST['caja'];
+        $id = $_POST['id'];
+        $hash = hash("SHA256", $clave);
+        if (empty($usuario) || empty($nombre) || empty($caja)) {
+            $msg = array('msg' => 'Todo los campos son obligatorios', 'icono' => 'warning');
+        }else{
+            if ($id == "") {
+                if($clave != $confirmar){
+                    $msg = array('msg' => 'Las contraseña no coinciden', 'icono' => 'warning');
+                }else{
+                    $data = $this->model->registrarUsuario($usuario, $nombre, $hash, $caja);
+                    if ($data == "ok") {
+                        $msg = array('msg' => 'Usuario registrado con éxito', 'icono' => 'success');
+                    }else if($data == "existe"){
+                        $msg = array('msg' => 'El usuario ya existe', 'icono' => 'warning');
+                    }else{
+                        $msg = array('msg' => 'Error al registrar el usuario', 'icono' => 'error');
+                    }
+                }
+            }else{
+                $data = $this->model->modificarUsuario($usuario, $nombre, $caja, $id);
+                if ($data == "modificado") {
+                    $msg = array('msg' => 'Usuario modificado con éxito', 'icono' => 'success');
+                }else {
+                    $msg = array('msg' => 'Error al modificar el usuario', 'icono' => 'error');
+                }
+            }
         }
-
-        print_r($data);
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
         die();
-    }*/
+    }
+
+    public function editar(int $id)
+    {
+        $data = $this->model->editarUser($id);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    public function eliminar(int $id)
+    {
+        $data = $this->model->accionUser(0, $id);
+        if ($data == 1) {
+            $msg = array('msg' => 'Usuario dado de baja', 'icono' => 'success');
+        }else{
+            $msg = array('msg' => 'Error al eliminar el usuario', 'icono' => 'error');
+        }
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+        die();
+    }
 }
--- Views/Clientes/index.php
+++ Views/Clientes/index.php
@@ -1,37 +1,51 @@
 <?php include "Views/Templates/header.php";?>
 
-<div id="container">
-    <section>
-        <div class="container">
-            <form id="frmClientes">
-                <div class="form-group mt-2">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control mb-2" id="nombre" placeholder="Ingrese el Nombre">
-                </div>
-                <div class="form-group">
-                    <label for="apellido">Apellido</label>
-                    <input type="text" class="form-control mb-2" id="apellido" placeholder="Ingrese el Apellido">
-                </div>
-                <div class="form-group">
-                    <label for="cedula">Cedula</label>
-                    <input type="number" class="form-control mb-2" id="cedula" placeholder="Ingrese la Cedula">
-                </div>
-                <div class="form-group">
-                    <label for="telefono">Telefono</label>
-                    <input type="tel" class="form-control mb-2" id="telefono" placeholder="Ingrese el Telefono">
-                </div>
-                <div class="form-group">
-                    <label for="correo">Correo</label>
-                    <input type="email" class="form-control mb-2" id="correo" aria-describedby="emailHelp" placeholder="Ingrese el correo">
-                </div>
-                <div class="form-group">
-                    <label for="direccion">Direccion</label>
-                    <input type="text" class="form-control mb-2" id="direccion" placeholder="1234 Main St">
-                </div>
-                <button type="submit" class="btn btn-primary" onclick="frmClientes(event);">Submit</button>
-            </form>
+<div class="card">
+    <div class="card-header card-header-primary">
+        Clientes
+    </div>
+    <div class="card-body">
+        <button class="btn btn-primary mb-2" type="button" onclick="frmClientes();"><ion-icon name="add-outline"></ion-icon></button>
+        <table class="table table-light table-bordered table-hover" id="tblClientes">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Cedula</th>
+                    <th scope="col">Teléfono</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Dirección</th>
+                    <th scope="col">Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="nuevo_cliente" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title" id="title">Nuevo Cliente</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="frmClientes">
+                    <div class="form-floating mb-3">
+                        <input id="nombre" class="form-control" type="text" name="nombre" placeholder="Nombre del nombre">
+                        <label for="nombre">Nombre</label>
+                    </div>
+    
+                    <button class="btn btn-primary" type="button" onclick="registrarCliente(event);" id="btnAccion">Registrar</button>
+                    <button class="btn btn-danger" type="button" data-bs-dismiss="modal">Cancelar</button>
+                </form>
+            </div>
         </div>
-    </section>
+    </div>
 </div>
 
 <?php include "Views/Templates/footer.php";?>
