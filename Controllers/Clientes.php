9ba485294b7740e2ca147986b1765cb13e4c4cd6
diegomorane <diegomorane>
2022-05-17 15:43:34 -0500

    Se añadieron funciones para los clientes

--- Controllers/Clientes.php
+++ Controllers/Clientes.php
@@ -14,6 +14,32 @@ class Clientes extends Controller
     public function insertar()
     {
         $data = $this->model->setClientes("pepe", "aguilar", 12837495, 982734819, "pepe@gmail.com", "Totiuco");
+        $data = $this->model->setClientes("jose", "almeida", 12837495, 982734819, "jose@gmail.com", "Chaguarquingo");
+        $data = $this->model->setClientes("lucho", "espinoza", 12837495, 982734819, "luchito@gmail.com", "Carapungo");
+        print_r($data);
+    }
+
+    public function verCliente($id)
+    {
+        $data = $this->model->getCliente($id);
+        print_r($data);
+    }
+
+    public function actualizar()
+    {
+        $data = $this->model->updateClientes(5, "Pepe", "Aguilar", 12837496, 982734818, "pepe@gmail.com", "Totiuco");
+        print_r($data);
+    }
+
+    public function verClientes()
+    {
+        $data = $this->model->getClientes();
+        print_r($data);
+    }
+
+    public function eliminarCliente($id)
+    {
+        $data = $this->model->deleteCliente($id);
         print_r($data);
     }
 }
