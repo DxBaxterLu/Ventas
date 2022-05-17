d3318465f587dcb45a853b296bf16ec3a94ede6a
diegomorane <diegomorane>
2022-05-17 14:33:02 -0500

    Se agregaron consultas query's

--- Config/App/Query.php
+++ Config/App/Query.php
@@ -1,7 +1,7 @@
 <?php
 class Query extends Conexion
 {
-    private $pdo, $con, $sql;
+    private $pdo, $con, $sql, $arrValues;
 
     public function __construct()
     {
@@ -19,5 +19,50 @@ class Query extends Conexion
         $data = $result->fetchAll(PDO::FETCH_ASSOC);
 
         return $data;
+    }
+
+    public function selectAll(string $sql)
+    {
+        $this->sql = $sql;
+
+        $result = $this->con->prepare($this->sql);
+        $result->execute();
+
+        $data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        return $data;
+    }
+
+    public function insert(string $sql, array $arrValues)
+    {
+        $this->sql = $sql;
+        $this->arrValues = $arrValues;
+
+        $insert = $this->conexion->prepare($this->sql);
+        $resInsert = $insert->execute($this->arrValues);
+
+        if ($resInsert) {
+            $lastInsert = $this->con->lastInsertId();
+        } else {
+            $lastInsert = 0;
+        }
+        return $lastInsert;
+    }
+
+    public function update(string $sql, array $arrValues)
+    {
+        $this->sql = $sql;
+        $this->arrValues = $arrValues;
+        $update = $this->con->prepare($this->sql);
+        $resExecute = $update->execute($this->arrValues);
+        return $resExecute;
+    }
+
+    public function delete(string $sql)
+    {
+        $this->sql = $sql;
+        $result = $this->con->prepare($this->sql);
+        $result->execute();
+        return $result;
     }
 }
