a085313e56036c98ac9f8f35979a8d562b9ffd16
Baxter <dave.f7.mart@gmail.com>
2022-05-16 22:32:32 -0500

    Se modifico la vista de concesionarios

--- Views/Concesionarios/index.php
+++ Views/Concesionarios/index.php
@@ -5,30 +5,19 @@
         <div class="container">
             <form id="frmClientes">
                 <div class="form-group mt-2">
+                    <label for="sector">Sector</label>
+                    <input type="text" class="form-control mb-2" id="sector" placeholder="Ingrese el Sector">
+                </div>
+                <div class="form-group">
                     <label for="nombre">Nombre</label>
                     <input type="text" class="form-control mb-2" id="nombre" placeholder="Ingrese el Nombre">
-                </div>
-                <div class="form-group">
-                    <label for="apellido">Apellido</label>
-                    <input type="text" class="form-control mb-2" id="apellido" placeholder="Ingrese el Apellido">
-                </div>
-                <div class="form-group">
-                    <label for="cedula">Cedula</label>
-                    <input type="number" class="form-control mb-2" id="cedula" placeholder="Ingrese la Cedula">
                 </div>
                 <div class="form-group">
                     <label for="telefono">Telefono</label>
                     <input type="tel" class="form-control mb-2" id="telefono" placeholder="Ingrese el Telefono">
                 </div>
-                <div class="form-group">
-                    <label for="correo">Correo</label>
-                    <input type="email" class="form-control mb-2" id="correo" aria-describedby="emailHelp" placeholder="Ingrese el correo">
-                </div>
-                <div class="form-group">
-                    <label for="direccion">Direccion</label>
-                    <input type="text" class="form-control mb-2" id="direccion" placeholder="1234 Main St">
-                </div>
-                <button type="submit" class="btn btn-primary" onclick="frmClientes(event);">Submit</button>
+
+                <button type="submit" class="btn btn-primary" onclick="frmConcesionarios(event);">Submit</button>
             </form>
         </div>
     </section>
